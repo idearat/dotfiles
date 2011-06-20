88d13a7a0ffc334125ce67b73dde6f2ad04bb399
Scott Shattuck <ss@technicalpursuit.com>
2011-06-19 19:36:20 -0600

    Added install.sh script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ln -sfFv ~/dotfiles/bash_profile ~/.bash_profile
+ln -sfFv ~/dotfiles/bashrc ~/.bashrc 
+ln -sfFv ~/dotfiles/gitconfig ~/.gitconfig 
+ln -sfFv ~/dotfiles/irbrc ~/.irbrc 
+ln -sfFv ~/dotfiles/rvmrc ~/.rvmrc 
+ln -sfFv ~/dotfiles/screenrc ~/.screenrc 
+ln -sfFv ~/dotfiles/tibetrc ~/.tibetrc 
+ln -sfFv ~/dotfiles/vim ~/.vim 
+ln -sfFv ~/dotfiles/vimrc ~/.vimrc 
