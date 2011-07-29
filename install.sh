dd90ea758fe16c6c77ac705e7e6820a75a48f213
Scott Shattuck <ss@technicalpursuit.com>
2011-07-29 16:44:01 -0600

    Fixed bash completion. Added cdtpi.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ln -sfv ~/dotfiles/bash_profile ~/.bash_profile
 ln -sfv ~/dotfiles/bashrc ~/.bashrc 
-ln -sfv ~/dotfiles/.git-flow-completion.bash ~/.git-flow-completion.sh
+ln -sfv ~/dotfiles/git-flow-completion.bash ~/.git-flow-completion.sh
 ln -sfv ~/dotfiles/gitconfig ~/.gitconfig 
 ln -sfv ~/dotfiles/gitignore ~/.gitignore 
 ln -sfv ~/dotfiles/irbrc/irbrc ~/.irbrc 
