aaa6c665030191a88058a388b658aa962943e6ae
idearat <ss@technicalpursuit.com>
2014-03-17 10:31:35 -0600

    Rebuild dotfiles implementation w/zsh, tmux, vim, etc.

--- install.sh
+++ install.sh
@@ -1,133 +1,128 @@
 #!/bin/bash
-#
 
-# Installs the basic links etc.
+# Installs the basic links etc. needed to leverage the dotfile setup.
 
-# NOTE: this assumes you're in the dotfiles directory when you run install.sh
-export DOTFILE_HOME="$HOME/.dotfiles"
+export DOTFILES="$HOME/.dotfiles"
 
 # Build working directories.
+
 mkdir ~/dev > /dev/null
 mkdir ~/tmp > /dev/null
 
-# Prerequisites / Utilities
-brew install ack
-brew install bash-completion
-brew install ctags
-brew install reattach-to-user-namespace
-brew install python # for pip/powerline etc.
+# Install and activate ZSH.
+brew install zsh
+curl -L http://install.ohmyz.sh | sh
 
-# install macvim/vim and link macvim into Applications dir
+# Install command-line utilities.
+brew install ack
+brew install ag     # not using yet, claimed better than ack for code
+brew install wget   # some cut/paste installs use wget vs. curl.
+
+brew install python # for pip, powerline, etc.
+
+# Install TMUX support and MacOSX patches for clipboard.
+brew install tmux
+brew install reattach-to-user-namespace
+
+# Install the node version manager and use it to install node.js.
+#brew install nvm
+#nvm install 0.10
+
+# Install PhantomJS for web testing.
+#brew install phantomjs
+
+# Patch up Apache legacy links to match historical aliases.
+# Apache on OSX 10.8 + doesn't provide the older standard locations. link them.
+sudo ln -s /Library/WebServer/Documents /etc/apache2/htdocs
+sudo ln -s /Library/WebServer/CGI-Executables /etc/apache2/cgi-bin
+
+# Install rbenv (rather than rvm) and use it to install Ruby.
+#brew install rbenv
+#brew install ruby-build
+#rbenv install 1.9.3-p0
+#rbenv local 1.9.3-p0
+#rbenv rehash
+
+# Install (Mac)VIM and link MacVIM into Applications dir.
 brew install macvim --override-system-vim
 brew linkapps
 
-# Build the command-t plugin properly so it will load in VIM
-pushd ~/.dotfiles/vim/janus.local/command-t/ruby/command-t
+# Install CTAGS to provide support for jstags operation.
+brew install ctags
+
+# Build the command-t plugin properly so it will load in VIM (after Ruby)
+pushd ${DOTFILES}/vim/vim/bundle/command-t/ruby/command-t
 ruby extconf.rb
 make
 popd
 
-# Apache on OSX 10.8 + doesn't provide the older standard locations for apache.
-sudo ln -s /Library/WebServer/Documents /etc/apache2/htdocs
-sudo ln -s /Library/WebServer/CGI-Executables /etc/apache2/cgi-bin
+# gh (GitHub CLI)
+brew tap jingweno/gh
+brew install gh
+brew install --build-from-source gh # build gh from source
+brew install --build-from-source --HEAD gh # build gh HEAD from source
 
-# Bash
-mv ~/.bash_profile ~/.bash_profile_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/bash/bash_profile ~/.bash_profile
+# ACK
+mv ~/.ackrc ~/.ackrc_orig > /dev/null
+ln -sfv ${DOTFILES}/ack/ackrc ~/.ackrc
 
-[[ -e ~/.bashrc ]] && \cp ~/.bashrc ~/.localrc
-mv ~/.bashrc ~/.bashrc_orig > /dev/null
-mv ~/.localrc ~/.localrc_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/bash/bashrc ~/.bashrc
-
-mv ~/.login.before ~/.login.before_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/bash/login.before ~/.login.before
-mv ~/.login.after ~/.login.after_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/bash/login.after ~/.login.after
-mv ~/.local.before ~/.local.before_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/bash/local.before ~/.local.before
-mv ~/.local.after ~/.local.after_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/bash/local.after ~/.local.after
-
-# Bin
+# local bin
 mv ~/bin.local ~/bin.local_orig > /dev/null
-ln -sv ${DOTFILE_HOME}/bin ~/bin.local
+ln -sfv ${DOTFILES}/bin ~/bin.local
 
-# Git
-ln -sfv ${DOTFILE_HOME}/git/git-flow-completion/git-flow-completion.bash ~/.git-flow-completion.bash
-mv ~/.gitignore ~/.gitignore_orig > /dev/null
-\cp ${DOTFILE_HOME}/git/gitignore.idearat ~/.gitignore
+# git
 mv ~/.gitconfig ~/.gitconfig_orig > /dev/null
-\cp -sfv ${DOTFILE_HOME}/git/gitconfig.idearat ~/.gitconfig
+\cp -sfv ${DOTFILES}/git/gitconfig.idearat ~/.gitconfig
 
-git config --global core.excludesfile ~/.gitignore
+mv ~/.gitignore ~/.gitignore_orig > /dev/null
+\cp ${DOTFILES}/git/gitignore.idearat ~/.gitignore
 
-# Node
+mv ~/.gitmessage.txt ~/.gitmessage.txt_orig > /dev/null
+\cp -sfv ${DOTFILES}/git/gitmessage.txt.idearat ~/.gitmessage.txt
 
-# TODO: ensure proper installation of nvm, then node
-
-# Python
-# NB: this is a patch file, necessary for things like pip installs. It shouldn't
-# be in place full-time if you want things to work right, but it must be for
-# some steps
-#mv ~/.pydistutils.cfg ~/.pydistutils.cfg_orig > /dev/null
-#\cp -sfv ${DOTFILE_HOME}/utils/pydistutils.cfg ~/.pydistutils.cfg
-
-#pip install --user git+git://github.com/Lokaltog/powerline
-
-# Ruby
-
-# TODO: remove rvm, replace with rbenv, and install.
-# NOTE that installing pre-1.9.x versions requires old GCC from
-# https://github.com/kennethreitz/osx-gcc-installer/downloads
+# ruby
 
 mv ~/.irbrc ~/.irbrc_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/ruby/irbrc/irbrc ~/.irbrc
+ln -sfv ${DOTFILES}/ruby/irbrc ~/.irbrc
+
 mv ~/.rdebugrc ~/.rdebugrc_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/ruby/rdebugrc ~/.rdebugrc
+ln -sfv ${DOTFILES}/ruby/rdebugrc ~/.rdebugrc
 
 # TIBET
 mv ~/.tibetrc ~/.tibetrc_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/tibet/tibetrc ~/.tibetrc
+ln -sfv ${DOTFILES}/tibet/tibetrc ~/.tibetrc
 
-# Utils
-mv ~/.ackrc ~/.ackrc_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/utils/ackrc ~/.ackrc
-mv ~/.screenrc ~/.screenrc_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/utils/screenrc ~/.screenrc
+# TMUX
 mv ~/.tmux.conf ~/.tmux.conf_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/utils/tmux.conf ~/.tmux.conf
+ln -sfv ${DOTFILES}/tmux/tmux.conf ~/.tmux.conf
 
 # VIM
-
-# /usr/local/share doesn't always have a vim reference.
-#ln -sfv /usr/share/vim /usr/local/share/vim
-
-# TODO add flag to turn this off as desired.
-# Initialize submodules. Janus etc. must be updated before we can link them.
-git submodule init
-git submodule update
-pushd vim/janus
-rake
-popd
-
 mv ~/.vim ~/.vim_orig > /dev/null
-ln -sFv ${DOTFILE_HOME}/vim/janus/janus/vim ~/.vim
-mv ~/.janus ~/.janus_orig > /dev/null
-ln -sFv ${DOTFILE_HOME}/vim/janus.local ~/.janus
+ln -sFv ${DOTFILES}/vim/vim ~/.vim
 
 mv ~/.vimrc ~/.vimrc_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/vim/janus/janus/vim/vimrc ~/.vimrc
+ln -sfv ${DOTFILES}/vim/vimrc ~/.vimrc
+
 mv ~/.vimrc.after ~/.vimrc.after_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/vim/vimrc.after ~/.vimrc.after
-mv ~/.vimrc.before ~/.vimrc.before_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/vim/vimrc.before ~/.vimrc.before
-mv ~/.vimrc.final ~/.vimrc.final_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/vim/vimrc.final ~/.vimrc.final
+ln -sfv ${DOTFILES}/vim/vimrc.after ~/.vimrc.after
 
 mv ~/.gvimrc ~/.gvimrc_orig > /dev/null
-ln -sfv ${DOTFILE_HOME}/vim/janus/janus/vim/gvimrc ~/.gvimrc
+ln -sfv ${DOTFILES}/vim/vim/gvimrc ~/.gvimrc
 
-# TODO: add echo for installing Command-T (gotta build it)
+mv ~/.editorconfig ~/.editorconfig_orig > /dev/null
+ln -sfv ${DOTFILES}/vim/editorconfig ~/.editorconfig
 
+# TODO: powerline install
+# git clone ...
+# TODO: powerline fonts etc. etc.
+# git clone ...
+# install font, configure font
 
+# ZSH
+mv ~/.local.after ~/.local.after_orig > /dev/null
+ln -sfv ${DOTFILES}/bash/local.after ~/.local.after
+
+# TODO: echo set of 'post-install hints'
+
+# if you want to make zsh your default shell...
+#
